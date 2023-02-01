e195a2d20654a428ede79b432a4985ba9905bc2a
Pernell Styles Jr <82741858+Stylesp007@users.noreply.github.com>
2023-02-01 00:49:04 -0500

    created anime grail App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Components/Header"
+import './Header.css'
+import '../node_modules/rsuite/dist/rsuite.min.css';
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,51 @@
+
+import { Modal, Button } from 'rsuite';
+import { useState } from "react";
+  
+
+
+export default function Header() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return(
+      <>
+    <header className="Header">
+      <h1>Anime Grail</h1>
+      <div className="right-header">
+        <Button onClick={handleOpen}>Watchlist</Button>
+        <h3 onClick={()=>console.log('favorites clicks')}>favorites</h3>
+        <h3 onClick={()=>console.log('recomendations clicks')}>Recomendations</h3>
+      </div>
+    </header>
+     <div style={{ margin: 50, textAlign: "center" }}>
+     <Modal
+         open={open}
+         onClose={handleClose}
+         backdrop="static"
+         >
+         <Modal.Header>
+             GeeksforGeeks
+         </Modal.Header>
+         <Modal.Body>
+             <p>
+                 Hi ! Geek <br /> This is a 
+                 <b>static</b> backdrop modal. 
+             </p>
+         </Modal.Body>
+       <Modal.Footer>
+             <Button onClick={handleClose} 
+               appearance="subtle">
+               Ok
+           </Button>
+           <Button onClick={handleClose} 
+               appearance="primary">
+               Cancel
+           </Button>
+       </Modal.Footer>
+   </Modal>
+</div>
+      </>
+  )
+}
